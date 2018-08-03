09c4f798721ed7503809a9f4f7da5913461bf27f
MalenaZP <malenazp@gmail.com>
2018-08-03 00:00:25 -0500

    creand data de restaurantes

---
+++ mapjs/data.js
@@ -0,0 +1,116 @@
+const data = {
+    'vegetarian': [
+        {
+            'name': 'La Casa de Bea',
+            'adress': 'Av. Flora Tristan 777, La Molina 15012',
+            'range': 'S/25',
+            'photo': 'https://d1mvj2ulps5lli.cloudfront.net/venues/1024/75/29/hcmp75298_436693.jpeg'
+        },
+        {
+            'name': 'Nanka',
+            'adress': 'Jr. Los Bambúes 198 (a la espalda del CC Molina Plaza)',
+            'range': 'S/28',
+            'photo': 'http://www.netlima.com/peru/7701.jpg'
+        },
+        {
+            'name': 'BioCafeta',
+            'adress': 'Parque Municipal de Barranco',
+            'range': 'S/22',
+            'photo': 'https://yt3.ggpht.com/a-/ACSszfH6cJouRYuMsgPfDz_H-fX4RDK-Wi9ad-Hzyg=s900-mo-c-c0xffffffff-rj-k-no'
+        },
+        {
+            'name': 'Edo Sushi Bar',
+            'adress': 'Calle las Caobas 180 - La Molina',
+            'range': 'S/18',
+            'photo': 'https://edosushibar.com/app/uploads/2017/06/alaska-meta-slider.jpg'
+        },
+        {
+            'name': 'Saint Tropez Restaurante',
+            'adress': 'Av. Javier Prado Este 5370 La Molina,',
+            'range': 'S/23',
+            'photo': 'https://qpr2l7.imagenii.com/static1.seesainttropez.com/image_uploader/photos/16/large/rivea-at-byblos-saint-tropez-terrace-saint-tropez.jpg?f=q(v=.8)&auth=1e918f82e5e5a2c4ffc98093ac21a1c2c4ed6ac8'
+        },
+        {
+            'name': 'Tierra Santa',
+            'adress': 'Av. Caminos del Inca 1512 - Surco',
+            'range': 'S/26',
+            'photo': 'https://content.emarket.pe/uploads/sites/22/2015/03/1400000015353.jpg'
+        }
+    ],
+    'buffet': [
+        {
+            'name': 'La Bistecca',
+            'adress': 'Av. Primavera 545 - San Borja',
+            'range': 'S/33',
+            'photo': 'https://static.wixstatic.com/media/a7ea1a_3e85cf4d45614318af833bf6d8152663.png/v1/fill/w_784,h_520,al_c,usm_0.66_1.00_0.01/a7ea1a_3e85cf4d45614318af833bf6d8152663.png'
+        },
+        {
+            'name': 'Restaurant Buffet Mandarín',
+            'adress': 'Av. Javier Prado Este 1860 - San Borja',
+            'range': 'S/45',
+            'photo': 'https://vice-images.vice.com/images/articles/meta/2016/07/21/one-man-tests-the-limits-of-mandarin-chinese-infamous-all-you-can-eat-buffet-1469116204.jpg?crop=1xw:0.7848837209302325xh;center,center&resize=1200:*'
+        },
+        {
+            'name': 'Puro Perú Buffet Gourmet',
+            'adress': 'Av. República de Panamá 258 - Surco',
+            'range': 'S/40',
+            'photo': 'http://puroperu.com.pe/images/slider_home/3.jpg'
+        },
+        {
+            'name': 'La Piccolina',
+            'adress': 'Av La Encalada 895 - Surco',
+            'range': 'S/45',
+            'photo': 'https://www.atrapalo.pe/common/photo/res/47856/0/ticr_890_370.jpg'
+        },
+        {
+            'name': 'El Rinconcito Escondido',
+            'adress': 'Parque Hernan Velarde 71 - La Victoria',
+            'range': 'S/35',
+            'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/04/5f/64/ec/restaurante-naia-hotel.jpg'
+        },
+        {
+            'name': 'My Home',
+            'adress': 'Av. la Molina 1120 - La Molina',
+            'range': 'S/42',
+            'photo': 'http://3.bp.blogspot.com/-2kt3I2lVTT4/T6BGR4YXLBI/AAAAAAAACNg/NKZTUMCuuNM/s1600/Bore+4+Restaurant+Kristina+2+2011-08-13.jpg'
+        }
+    ],
+    'criollo': [
+        {
+            'name': 'Panchita',
+            'adress': 'Calle 2 de Mayo 298 - Miraflores',
+            'range': 'S/25',
+            'photo': 'https://www.atrapalo.pe/common/photo/res/20316/0/ticr_890_370.jpg'
+        },
+        {
+            'name': 'El Señorío Criollo',
+            'adress': 'José Gálvez 224 - Lima',
+            'range': 'S/18',
+            'photo': 'https://www.atrapalo.pe/common/photo/res/18999/142950/ticr_0_0.jpg'
+        },
+        {
+            'name': 'Restaurant Sabores Peruanos',
+            'adress': 'Lima, Calle 18',
+            'range': 'S/27',
+            'photo': 'https://www.atrapalo.pe/common/photo/res/46994/0/ticr_890_370.jpg'
+        },
+        {
+            'name': 'Restaurante Buffet La Chola Criolla',
+            'adress': ' Av Nicolás Arriola 401 - Vitarte',
+            'range': 'S/22',
+            'photo': 'http://photos1.blogger.com/blogger/6844/1734/1600/IMGP1263-40.jpg'
+        },
+        {
+            'name': 'El Embrujo',
+            'adress': 'Calle Berlin 536 - Miraflores',
+            'range': 'S/26',
+            'photo': 'https://www.gediscovery.edu.pe/blog/wp-content/uploads/2017/07/gediscovery-gastronomia-regiones.jpg'
+        },
+        {
+            'name': 'Aromas Peruanos',
+            'adress': 'Av. Guardia Civil 856 - San Borja',
+            'range': 'S/27',
+            'photo': 'http://blog.trip-peru.com/wp-content/uploads/2012/09/Peruvian-Gastronomy-Boom-Trip-Peru.jpg'
+        }
+    ]
+};

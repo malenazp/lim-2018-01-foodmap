903ca509df0aa2d9dd4a5194655d75a6f9a58409
MalenaZP <malenazp@gmail.com>
2018-08-03 01:25:10 -0500

    solucionando diferencias localhost ghpages

--- splash/splash.js
+++ splash/splash.js
@@ -1,6 +1,6 @@
 $(window).on('load', function () {
     setTimeout(function () {
-    const $preloader = $('#page-preloader'),
+    const $preloader = $('#splash'),
         $spinner = $preloader.find('.spinner');
     $spinner.fadeOut();
         $preloader.delay(350).fadeOut('slow', function () {
